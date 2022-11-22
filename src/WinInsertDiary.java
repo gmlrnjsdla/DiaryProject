26b4d557022f67f3b3c4aa2d9835616964b1eafb
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-11-22 16:00:00 +0900

    다이어리 제작

---
+++ src/WinInsertDiary.java
@@ -0,0 +1,224 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class WinInsertDiary extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField tfDate;
+	private JTextField tfTitle;
+	private JTextField tfId;
+	private JTextField tfPw;
+	private JLabel lblpic;
+	private ImageIcon icon;
+	private JButton okButton;
+
+	
+
+	/**
+	 * Create the dialog.
+	 */
+	public WinInsertDiary() {
+		setTitle("일기장");
+		setBounds(100, 100, 412, 552);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("날짜 : ");
+		lblNewLabel.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(28, 57, 57, 15);
+		contentPanel.add(lblNewLabel);
+		
+		tfDate = new JTextField();
+		tfDate.setBounds(102, 54, 116, 21);
+		contentPanel.add(tfDate);
+		tfDate.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("날씨 : ");
+		lblNewLabel_1.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(28, 121, 57, 15);
+		contentPanel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("제목 : ");
+		lblNewLabel_2.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(28, 177, 57, 15);
+		contentPanel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("내용 : ");
+		lblNewLabel_3.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(28, 227, 57, 15);
+		contentPanel.add(lblNewLabel_3);
+		
+		
+		
+		JComboBox cbWeather = new JComboBox();
+		cbWeather.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				icon = new ImageIcon(".\\weather\\"+cbWeather.getSelectedItem()+".png");
+				Image img = icon.getImage();
+				Image ChangeImg = img.getScaledInstance(130, 106,Image.SCALE_SMOOTH);
+				ImageIcon changeIcon = new ImageIcon(ChangeImg);
+				lblpic.setIcon(changeIcon);
+			}
+		});
+		cbWeather.setModel(new DefaultComboBoxModel(new String[] {"선택...", "맑음", "흐림", "비", "바람", "번개", "눈"}));
+		cbWeather.setBounds(106, 117, 85, 23);
+		contentPanel.add(cbWeather);
+		
+		lblpic = new JLabel("");
+		lblpic.setBackground(UIManager.getColor("Button.disabledShadow"));
+		lblpic.setBounds(230, 86, 136, 106);
+		lblpic.setOpaque(true);
+//		lblpic.setIcon(new ImageIcon(".\\weather\\맑음.png"));
+		contentPanel.add(lblpic);
+		
+		JButton btnNewButton = new JButton("...");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				WinCalendar winCalendar = new WinCalendar();
+				winCalendar.setModal(true);
+				winCalendar.setVisible(true);
+				tfDate.setText(winCalendar.getDate());
+			}
+		});
+		btnNewButton.setBounds(230, 53, 57, 23);
+		contentPanel.add(btnNewButton);
+		
+		tfTitle = new JTextField();
+		tfTitle.setColumns(10);
+		tfTitle.setBounds(102, 174, 116, 21);
+		contentPanel.add(tfTitle);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(102, 227, 233, 197);
+		contentPanel.add(scrollPane);
+		
+		JTextPane tfContent = new JTextPane();
+		scrollPane.setViewportView(tfContent);
+		
+		tfId = new JTextField();
+		tfId.setBounds(63, 449, 116, 21);
+		contentPanel.add(tfId);
+		tfId.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("아이디 : ");
+		lblNewLabel_4.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+		lblNewLabel_4.setBounds(12, 452, 57, 15);
+		contentPanel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("비밀번호 : ");
+		lblNewLabel_4_1.setFont(new Font("맑은 고딕", Font.BOLD, 13));
+		lblNewLabel_4_1.setBounds(191, 452, 72, 15);
+		contentPanel.add(lblNewLabel_4_1);
+		
+		tfPw = new JTextField();
+		tfPw.setToolTipText("입력 후 Enter");
+		tfPw.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+					int length = tfPw.getText().length();
+					if(length >= 4) {
+						String value = JOptionPane.showInputDialog("비밀번호 입력(확인)");
+						if(tfPw.getText().equals(value)) {
+							JOptionPane.showMessageDialog(null, "일치합니다.");
+							okButton.setEnabled(true);
+						}
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "4글자 이상 비밀번호 입력");
+						tfPw.setText("");
+						tfPw.requestFocus();
+					}
+				}
+			}
+		});
+		tfPw.setColumns(10);
+		tfPw.setBounds(255, 449, 116, 21);
+		contentPanel.add(tfPw);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				okButton = new JButton("쓰기");
+				okButton.setEnabled(false);
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						try {
+							Class.forName("com.mysql.cj.jdbc.Driver");
+							Connection conn = DriverManager.getConnection
+									("jdbc:mysql://localhost:3306/sqldb", "root","1234");
+							
+							String mdate = tfDate.getText();
+							String weather = (String)cbWeather.getSelectedItem();
+							String pic = "./weather/"+cbWeather.getSelectedItem()+".png";
+							String title = tfTitle.getText();
+							String content = tfContent.getText();
+							String id = tfId.getText();
+							String curpw = tfPw.getText();
+							
+							String sql = "INSERT INTO diarytbl(mdate, weather, pic, title, content, id, curpw) "
+									+ "VALUES('"+mdate+"','"+weather+"','"+pic+"','"+title+"','"+content+"','"+id+"','"+curpw+"')";
+							Statement stmt = conn.createStatement();
+							stmt.executeUpdate(sql);
+							
+							JOptionPane.showMessageDialog(null, "업로드 성공!");
+							
+							setVisible(false);
+							WinMain winMain = new WinMain();
+							winMain.setVisible(true);
+							
+							} catch (Exception e1) {
+							e1.printStackTrace();
+							}
+						
+					}
+				});
+//				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+//				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
